1a1bfa8fdc29240f180a24e7d7939557a0374579
E-Vargo <vargoelliott@gmail.com>
2022-09-01 19:27:15 -0700

    commit tree 67d791cc2a6e563a391b92b83fc27495d7e664f7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(array, callback) {
+    const r = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      const theElement = array[i];
+      r.push(callback(theElement));
+    }
+  
+    return r;
+  }
+
+  function reduce(array, callback, start){
+    let value = 0;
+    if (start){
+        value = start
+        for (let i = 0; i < array.length; i++) {
+            value = callback(array[i], value)
+        }
+        return value
+    } else {
+        value = array[0]
+        for (let i = 1; i < array.length; i++) {
+            value = callback(array[i], value);
+    }
+    return value
+  }
+}
